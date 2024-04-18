af30a098ed2c5510859a3f21e07bd4ab920f016d
YAMITRAO <work.aky@gmail.com>
2024-04-18 16:46:33 +0530

    link is added to aboutus page

--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,6 +1,17 @@
+import Link from "next/link"
+
 const aboutus = () => {
+    const details = [
+        { id : 1, name: 'Yash', role: 'Senior Developer'},
+        { id : 2, name: 'Vaibhav', role: 'Backend Developer'},
+        { id : 3, name: 'Suresh', role: 'Frontend Developer'}
+      ]
     return (
-      <h1>Know about us is here</h1>
+      <>
+      {details.map((val)=>{
+        return <div> <Link href={`/aboutus/${val.id}`}>{val.name}</Link></div>
+      })}
+      </>
     )
   }
   
