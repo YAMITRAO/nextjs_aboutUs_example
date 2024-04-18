3c9a88787f26abca234025159391af959ce1b5c5
YAMITRAO <work.aky@gmail.com>
2024-04-18 12:41:33 +0530

    project create with aboutus page

---
+++ pages/aboutus.js
@@ -0,0 +1,9 @@
+
+
+const aboutus = () => {
+  return (
+    <h1>Know about us is here</h1>
+  )
+}
+
+export default aboutus
---
+++ pages/news.js
@@ -0,0 +1,9 @@
+
+
+const news = () => {
+  return (
+    <h1>News Page here</h1>
+  )
+}
+
+export default news
